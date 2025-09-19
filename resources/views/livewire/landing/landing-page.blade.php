81e20470cbc93c895259ef5ca6fff5b387eec183
Wisnu Aji Pamungkas <wisnuajipamungkas354@gmail.com>
2025-09-19 20:16:14 +0700

    Finish Alasan Sekolah Di BUBS Section

--- resources/views/livewire/landing/landing-page.blade.php
+++ resources/views/livewire/landing/landing-page.blade.php
@@ -1,16 +1,16 @@
-<div class="my-8 mx-auto w-full md:max-w-[1024px] lg:max-w-[1200px] flex justify-between items-center">
-    <section class="px-4 md:px-12 flex flex-col items-center gap-[6rem] md:flex-row md:justify-between md:gap-0">
+<div class="my-8 mx-auto w-full md:max-w-[1024px] lg:max-w-[1200px] flex flex-col justify-between items-center gap-[10rem]">
+    <section class="px-4 md:px-12 flex flex-col items-center gap-[5rem] md:flex-row md:justify-between md:gap-0">
         <div class="flex flex-col gap-8 md:max-w-[50%]">
-            <h1 class="text-6xl/18 font-black text-accent-content">SMA <span class="text-[#008080]">Baitul Ulya</span> Boarding School</h1>
-            <p class="text-xl/10 text-[#747474]">sekolah adalah tempat mencetak penerus bangsa
+            <h1 class="text-4xl/12 lg:text-6xl/18 font-black text-accent-content">SMA <span class="text-[#008080]">Baitul Ulya</span> Boarding School</h1>
+            <p class="text-lg/8 md:text-xl/10 text-[#747474]">sekolah adalah tempat mencetak penerus bangsa
                 yang berkualitas dan berprestasi di segala bidang
                 yang dapat bersaing di dunia  internasional
             </p>
-            <div class="flex gap-6">
+            <div class="flex flex-col md:flex-row gap-6">
                 <button class="px-8 py-3 bg-[#008080] rounded-full text-white font-bold shadow-2xl">Lihat Selengkapnya ></button>
                 <button class="px-8 py-3 outline-2 outline-[#008080] rounded-full text-[#008080] font-bold shadow-2xl">Video</button>
             </div>
-            <div class="flex justify-between items-center">
+            <div class="flex flex-col gap-3 md:flex-row justify-between items-center">
                 <p>Kami hadir di Media Sosial :</p>
                 <span class="flex gap-4 text-2xl">
                     <a href="#"><i class="bi bi-facebook"></i></a> 
@@ -21,9 +21,45 @@
             </div>
         </div>
         <div class="relative flex justify-end">
-            <img src={{ asset('img/herosection.png') }} alt="Hero Section" class="mx-auto md:m-0 max-w-[70%] rounded-4xl object-contain shadow-xl" />
-            <span class="absolute right-6 top-8 md:-right-12 md:top-12 bg-white max-w-[12rem] text-sm shadow-xl rounded-xl p-3 font-semibold italic">“Jika Ingin Sukses Belajarlah bersabar”</span>
+            <img src={{ asset('img/herosection.png') }} alt="Hero Section" class="mx-auto md:m-0 md:max-w-[70%] rounded-4xl object-contain shadow-xl" />
+            <span class="absolute right-0 top-8 md:-right-12 md:top-12 bg-white max-w-[12rem] text-sm shadow-xl rounded-xl p-3 font-semibold italic">“Jika Ingin Sukses Belajarlah bersabar”</span>
             <span class="absolute left-0 bottom-12  md:left-12 md:bottom-12 max-w-[14rem] text-sm bg-white p-3 rounded-xl shadow-xl italic font-semibold">“Belajar Itu Menyenangkan kuncinya jangan pernah menyerah oleh apapun itu”</span>
+        </div>
+    </section>
+    <section class="px-4 md:px-12 flex flex-col items-center gap-[5rem] md:justify-between md:gap-8">
+        <div class="flex flex-col gap-4">
+            <h3 class="mx-auto text-2xl/8 md:text-3xl/10 lg:text-4xl/12 text-accent-content font-extrabold max-w-[80%] md:max-w-[70%] text-center">Kenapa Harus <span class="text-[#008080]">SMA Baitul Ulya</span> Boarding School?</h3>
+            <p class="mx-auto md:text-lg/8 lg:text-xl/10 text-[#727272] max-w-[100%] md:max-w-[85%] text-center">Alasan kenapa harus memilih untuk bergabung dengan SMA Baitul Ulya Boarding School.</p>
+        </div>
+        <div class="flex flex-wrap gap-3 w-full justify-center">
+            <div class="flex flex-col gap-4 w-full sm:w-[16rem] bg-white shadow-2xl rounded-2xl py-18 px-3">
+                <span class="text-6xl text-[#727272] text-center">
+                    <i class="bi bi-pc-display"></i>
+                </span>
+                <h4 class="text-xl font-semibold text-center text-accent-content">Fasilitas Lengkap</h4>
+                <p class="text-lg text-[#727272] text-center">Penunjang belajar dengan kualitas terbaik</p>
+            </div>
+            <div class="flex flex-col gap-4 w-full sm:w-[16rem] bg-white shadow-2xl rounded-2xl py-18 px-3">
+                <span class="text-6xl text-[#727272] text-center">
+                    <i class="bi bi-building-fill-check"></i>
+                </span>
+                <h4 class="text-xl font-semibold text-center text-accent-content">Lingkungan Nyaman</h4>
+                <p class="text-lg text-[#727272] text-center">Berada di lingkungan yang nyaman dan asri</p>
+            </div>
+            <div class="flex flex-col gap-4 w-full sm:w-[16rem] bg-white shadow-2xl rounded-2xl py-18 px-3">
+                <span class="text-6xl text-[#727272] text-center">
+                    <i class="bi bi-building-fill-check"></i>
+                </span>
+                <h4 class="text-xl font-semibold text-center text-accent-content">Pengajar Kompeten</h4>
+                <p class="text-lg text-[#727272] text-center">Guru terbaik dengan pengalaman </p>
+            </div>
+            <div class="flex flex-col gap-4 w-full sm:w-[16rem] bg-white shadow-2xl rounded-2xl py-18 px-3">
+                <span class="text-6xl text-[#727272] text-center">
+                    <i class="bi bi-building-fill-check"></i>
+                </span>
+                <h4 class="text-xl font-semibold text-center text-accent-content">Kerja Sama Luas</h4>
+                <p class="text-lg text-[#727272] text-center">Dapat kesempatan kerja yang lebih terjamin</p>
+            </div>
         </div>
     </section>
 </div>
